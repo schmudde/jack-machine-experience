5db4cb4e164d7a6c75e5a196055030a4def59cbc
schmudde <schmudde@beyondthefra.me>
2017-11-12 22:09:48 -0500

    Update to state updates. Buttons working

--- src/jack_machine_experience/core.cljs
+++ src/jack_machine_experience/core.cljs
@@ -4,11 +4,11 @@
 
 ;;(enable-console-print!)
 
-(def direction (atom {:north cold-war :west welcome :south nil :east ww2}))
 (def welcome {:locale "Welcome" :locale-content "" :locale-bg ""})
 (def cold-war {:locale "NYC Mainframe" :locale-content "[[Animation] Mainframes: Missile Defense, Missile Guidance, Financial Transactions, Flight Scheduling, Atomic Research, Surveillance] Dominated by multinational corporations with deep corporate and government contacts: IBM, Sperry Rand, Burroughs, General Electric, and Honeywell, their flagship mainframes sold in the thousands and tens-of-thousands. These prestige products were accompanied by a range of number crunching, time keeping, and word processing products to outfit the rest of the office. Smaller business equipment suppliers, like Jack Tramiel's Commodore Business Machines, were tremendously disadvantaged. They built products that went direct to consumer, competing on price with the post-war global order their marketplace. Commodore ran lean and these handicaps became attributes as Tramiel's business philosophy began to align with the forthcoming technological revolution." :locale-bg "borderlessBG"})
 (def pc {:locale "Global PC" :locale-content "" :local-bg ""})
 (def ww2 {:locale "Lodz Tabulator" :locale-content "" :local-bg ""})
+(def direction (atom {:north nil :west welcome :south nil :east ww2}))
 
 ;; 1 - Create the broad dispatch, which is not the data iself
 
@@ -49,7 +49,7 @@
 
 ;; 5 - Subscribe to the query of the data and create the view, that way if it is updated, we update it.
 
-;; I want the bgvid video componenet to rerender when there is an update to :locale-bg
+;; TODO: I want the bgvid video componenet to rerender when there is an update to :locale-bg
 
 (defn screen-locale []
   [:div
@@ -61,9 +61,14 @@
     @(rf/subscribe [:locale])]
    [:p.sans-serif @(rf/subscribe [:locale-content])]])
 
-(defn available-directions [cardinal]
-  (when-let [direction-info (cardinal @direction)]
-    #(rf/dispatch [:screen-change direction-info])))
+(defn update-state! [cardinal]
+  (fn []
+    (when-let [content (cardinal @direction)]
+      (case cardinal
+        :east (reset! direction (assoc @direction :north cold-war))
+        :north (reset! direction (assoc @direction :south pc))
+        nil)
+      (rf/dispatch [:screen-change content]))))
 
 (defn screen-changer
   []
@@ -71,15 +76,14 @@
    [:div.flex.justify-center
    [:div.pt6.flex.items-center
     [:div {:style {:float "left"}}
-     [:button {:on-click (available-directions :west)} "←"]]
+     [:button {:on-click (update-state! :west)} "←"]]
     [:div {:style {:float "left"}}
-     [:button {:on-click (available-directions :north)
+     [:button {:on-click (update-state! :north)
                :style {:display "block"}} "↑"]
-     [:button {:on-click (available-directions :south)
+     [:button {:on-click (update-state! :south)
                :style {:display "block"}}"↓"]]
     [:div {:style {:float "left"}}
-     [:button {:on-click (available-directions :east)} "→"]]]]])
-
+     [:button {:on-click (update-state! :east)} "→"]]]]])
 
 (defn ui []
   [:div.pa4
