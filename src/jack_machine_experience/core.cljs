a9785fccbda13083d96f79b57cb8e8515550dc54
schmudde <d@schmud.de>
2017-11-19 19:26:44 -0500

    Media working

--- src/jack_machine_experience/core.cljs
+++ src/jack_machine_experience/core.cljs
@@ -4,11 +4,11 @@
 
 ;;(enable-console-print!)
 
-(def welcome {:locale "Welcome" :locale-content "" :locale-bg ""})
-(def cold-war {:locale "NYC Mainframe" :locale-content "[[Animation] Mainframes: Missile Defense, Missile Guidance, Financial Transactions, Flight Scheduling, Atomic Research, Surveillance] Dominated by multinational corporations with deep corporate and government contacts: IBM, Sperry Rand, Burroughs, General Electric, and Honeywell, their flagship mainframes sold in the thousands and tens-of-thousands. These prestige products were accompanied by a range of number crunching, time keeping, and word processing products to outfit the rest of the office. Smaller business equipment suppliers, like Jack Tramiel's Commodore Business Machines, were tremendously disadvantaged. They built products that went direct to consumer, competing on price with the post-war global order their marketplace. Commodore ran lean and these handicaps became attributes as Tramiel's business philosophy began to align with the forthcoming technological revolution." :locale-bg "borderlessBG"})
-(def pc {:locale "Global PC" :locale-content "" :local-bg ""})
-(def ww2 {:locale "Lodz Tabulator" :locale-content "" :local-bg ""})
-(def direction (atom {:north nil :west welcome :south nil :east ww2}))
+(def welcome {:locale "Welcome" :locale-content "" :locale-bg "commodore"})
+(def cold-war {:locale "NYC Mainframe" :locale-content "[[Animation] Mainframes: Missile Defense, Missile Guidance, Financial Transactions, Flight Scheduling, Atomic Research, Surveillance] Dominated by multinational corporations with deep corporate and government contacts: IBM, Sperry Rand, Burroughs, General Electric, and Honeywell, their flagship mainframes sold in the thousands and tens-of-thousands. These prestige products were accompanied by a range of number crunching, time keeping, and word processing products to outfit the rest of the office. Smaller business equipment suppliers, like Jack Tramiel's Commodore Business Machines, were tremendously disadvantaged. They built products that went direct to consumer, competing on price with the post-war global order their marketplace. Commodore ran lean and these handicaps became attributes as Tramiel's business philosophy began to align with the forthcoming technological revolution." :locale-bg "ibm"})
+(def pc {:locale "Global PC" :locale-content "" :locale-bg "pet"})
+(def ww2 {:locale "Lodz Tabulator" :locale-content "" :locale-bg "dehomag" :locale-puzzle 1})
+(def direction (atom {:north nil :west ww2 :south nil :east welcome}))
 
 ;; 1 - Create the broad dispatch, which is not the data iself
 
@@ -22,7 +22,7 @@
 (rf/reg-event-db
  :initialize
  (fn [_ _]
-   {:locale "Jack and the Machine" :locale-content "" :locale-bg ""
+   {:locale "Jack and the Machine" :locale-content "" :locale-bg "jatm"
     :buttons {:up (:north @direction) :down (:south @direction)}}))
 
 (rf/reg-event-db
@@ -56,22 +56,28 @@
 
 ;; 5 - Subscribe to the query of the data and create the view, that way if it is updated, we update it.
 
-;; TODO: I want the bgvid video componenet to rerender when there is an update to :locale-bg
+(defn check-box []
+  [:div
+   ;;[:input {:type "check-box" :id "box-one" :name "box-two" :value @(rf/subscribe [:locale-puzzle])}]
+   ])
+
 
 (defn screen-locale []
   [:div
-   [:video {:src (str "vid/" @(rf/subscribe [:locale-bg]) ".ogg") :type "video/ogg"
-            :poster "TODO" :id "bgvid" :auto-play "autoplay" :loop true}
-    [:source {:src (str "vid/" @(rf/subscribe [:locale-bg]) ".webm") :type "video/webm;codecs='vp8, vorbis'"}]]
+   [:div.container
+    [:video.videoPlayer {:src (str "vid/" @(rf/subscribe [:locale-bg]) ".webm") :type "video/webm;codecs='vp8, vorbis'"
+                         :poster "TODO" :id "bgvid" :auto-play "autoplay" :loop true}
+     [:source {:src (str "vid/" @(rf/subscribe [:locale-bg]) ".ogg") :type "video/ogg"}]]]
    [:div.f-headline.lh-solid.tc
     @(rf/subscribe [:locale])]
-   [:p.sans-serif @(rf/subscribe [:locale-content])]])
+   (check-box)
+   [:p @(rf/subscribe [:locale-content])]])
 
 (defn update-state! [cardinal]
   (fn []
     (when-let [content (cardinal @direction)]
       (case cardinal
-        :east (reset! direction (assoc @direction :north cold-war))
+        :west (reset! direction (assoc @direction :north cold-war))
         :north (reset! direction (assoc @direction :south pc))
         nil)
       (rf/dispatch [:screen-change content]))))
