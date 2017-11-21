36f0f0ecaa4ddc3e1b6b063dd286ecc5aa5a81a8
schmudde <d@schmud.de>
2017-11-20 19:27:48 -0500

    Good enough for demo, kinda

--- src/jack_machine_experience/core.cljs
+++ src/jack_machine_experience/core.cljs
@@ -4,8 +4,10 @@
 
 (enable-console-print!)
 
+;; [[Animation] Mainframes: Missile Defense, Missile Guidance, Financial Transactions, Flight Scheduling, Atomic Research, Surveillance] Dominated by multinational corporations with deep corporate and government contacts: IBM, Sperry Rand, Burroughs, General Electric, and Honeywell, their flagship mainframes sold in the thousands and tens-of-thousands. These prestige products were accompanied by a range of number crunching, time keeping, and word processing products to outfit the rest of the office. Smaller business equipment suppliers, like Jack Tramiel's Commodore Business Machines, were tremendously disadvantaged. They built products that went direct to consumer, competing on price with the post-war global order their marketplace. Commodore ran lean and these handicaps became attributes as Tramiel's business philosophy began to align with the forthcoming technological revolution.
+
 (def welcome {:locale "Welcome" :cardinality :east :locale-content "" :locale-bg "commodore"})
-(def cold-war {:locale "NYC Mainframe" :cardinality :north :locale-content "[[Animation] Mainframes: Missile Defense, Missile Guidance, Financial Transactions, Flight Scheduling, Atomic Research, Surveillance] Dominated by multinational corporations with deep corporate and government contacts: IBM, Sperry Rand, Burroughs, General Electric, and Honeywell, their flagship mainframes sold in the thousands and tens-of-thousands. These prestige products were accompanied by a range of number crunching, time keeping, and word processing products to outfit the rest of the office. Smaller business equipment suppliers, like Jack Tramiel's Commodore Business Machines, were tremendously disadvantaged. They built products that went direct to consumer, competing on price with the post-war global order their marketplace. Commodore ran lean and these handicaps became attributes as Tramiel's business philosophy began to align with the forthcoming technological revolution." :locale-bg "ibm"})
+(def cold-war {:locale "NYC Mainframe" :cardinality :north :locale-content "" :locale-bg "ibm"})
 (def pc {:locale "Global PC" :cardinality :south  :locale-content "" :locale-bg "pet"})
 (def ww2 {:locale "Lodz Tabulator" :cardinality :west  :locale-content "" :locale-bg "dehomag" :locale-puzzle {:box-1 true :box-2 false :box-3 false}})
 (def direction (atom {:north nil :west ww2 :south nil :east welcome}))
@@ -24,7 +26,7 @@
  :initialize
  (fn [_ _]
    {:locale "Jack and the Machine" :cardinality nil :locale-content "" :locale-bg "jatm"
-    :buttons {:up (:north @direction) :down (:south @direction)}}))
+    :locale-puzzle nil :buttons {:up (:north @direction) :down (:south @direction)}}))
 
 (rf/reg-event-db
  :screen-change
@@ -80,12 +82,11 @@
 ;; 5 - Subscribe to the query of the data and create the view, that way if it is updated, we update it.
 
 (defn update-available-state! [cardinality]
-    ;;(println "Update available state to: " cardinality)
-      (case cardinality
-        :west (reset! direction (assoc @direction :north cold-war))
-        :north (reset! direction (assoc @direction :south pc))
-        nil)
-      )
+  (case cardinality
+    :west (reset! direction (assoc @direction :north cold-war))
+    :north (reset! direction (assoc @direction :south pc))
+    nil))
+
 
 (defn update-checked-state! [box-number content puzzle-solution]
   (fn [event]
@@ -93,7 +94,6 @@
       (if user-value
         (reset! checkboxes (assoc @checkboxes box-number true))
         (reset! checkboxes (assoc @checkboxes box-number false)))
-
       (when (= puzzle-solution @checkboxes) (update-available-state! (:cardinality content)))
       (rf/dispatch [:code-change @checkboxes])
       (rf/dispatch [:screen-change content])
@@ -103,14 +103,16 @@
 (defn check-box [cardinality]
   (let [puzzle-solution @(rf/subscribe [:locale-puzzle])
         content (cardinality @direction)]
-    [:div
+    [:div.bg-white.flex.items-center.w-50.center.pt1
+     [:div.center
      [:input {:type "checkbox" :id "box-1" :on-change (update-checked-state! :box-1 content puzzle-solution) }]
      [:input {:type "checkbox" :id "box-2" :on-change (update-checked-state! :box-2 content puzzle-solution) }]
-     [:input {:type "checkbox" :id "box-3" :on-change (update-checked-state! :box-3 content puzzle-solution) }]]))
+     [:input {:type "checkbox" :id "box-3" :on-change (update-checked-state! :box-3 content puzzle-solution) }]]]))
 
 
 (defn screen-locale []
-  (let [current-cardinality @(rf/subscribe [:cardinality])]
+  (let [current-cardinality @(rf/subscribe [:cardinality])
+        current-puzzle @(rf/subscribe [:locale-puzzle])]
   [:div
    [:div.container
     [:video.videoPlayer {:src (str "vid/" @(rf/subscribe [:locale-bg]) ".webm") :type "video/webm;codecs='vp8, vorbis'"
@@ -118,23 +120,26 @@
      [:source {:src (str "vid/" @(rf/subscribe [:locale-bg]) ".ogg") :type "video/ogg"}]]]
    [:div.f-headline.lh-solid.tc
     @(rf/subscribe [:locale])]
-   ;;[:p (str @(rf/subscribe [:cardinality]))]
-   (when current-cardinality (check-box current-cardinality))
-   [:p @(rf/subscribe [:locale-content])]]))
+   [:p @(rf/subscribe [:locale-content])]
+   (when current-puzzle
+     [:div.tc.pa3.mt4.bg-black-70.mw6 {:style {:margin-left "auto" :margin-right "auto"}}
+      (check-box current-cardinality)
+      [:p (str current-puzzle)]])
+   ]))
 
-(defn update-screen-state! [cardinal] ;; THIS IS NOT REALLY USED!
+(defn update-screen-state! [cardinal]
   (fn []
     (when-let [content (cardinal @direction)]
       (case cardinal
         ;;:west (reset! direction (assoc @direction :north cold-war))
-        :north (reset! direction (assoc @direction :south pc))
+        :north (reset! direction (assoc @direction :south pc)) ;; THIS IS ONLY USED FOR NORTH!
         nil)
       (rf/dispatch [:screen-change content]))))
 
 (defn screen-changer []
     [:div.w-100
      [:div.flex.justify-center
-      [:div.pt6.flex.items-center
+      [:div.pt2.flex.items-center
        [:div {:style {:float "left"}}
         [:button {:on-click (update-screen-state! :west)} "←"]]
        [:div {:style {:float "left"}}
@@ -148,7 +153,7 @@
         [:button {:on-click (update-screen-state! :east)} "→"]]]]])
 
 (defn ui []
-  [:div.pa4
+  [:div.pt4
    [:div.vh-50
     [screen-locale]]
    [screen-changer]])
